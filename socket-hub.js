cece7dd5e9e8eeca41a4329f8d43637a8ed410e0
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-11-15 18:07:53 +0200

    first commit

---
+++ socket-hub.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const logger = require('./event-logger');
+
+// creating the root namespace on the specified port 
+const io = require('socket.io')(port);
+io.on('connection', (socket)=>{
+    console.log(`socket ${socket.id} is connected to root`);
+});
+
+// /caps namespace:
+const caps = io.of('/caps');
+caps.on('connection', (socket)=>{
+    console.log(`socket ${socket.id} connected to /caps`);
+    socket.on('joinRoom', (roomName)=>{
+        socket.join(roomName);
+        console.log('socket joined room: ' + roomName);
+        socket.emit('joinedRoom', `Joined room ${roomName}`);
+    });
+
+    socket.on('pickup', payload => {
+        logger('pickup', payload);
+        caps.emit('d-pickup', payload);
+    });
+
+    socket.on('in-transit', payload => {
+        logger('in-transit', payload);
+    });
+
+    socket.on('delivered', payload => {
+        logger('delivered', payload);
+        caps.emit('delivered', payload);
+    });
+});
---
+++ vendor/vendor.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const io = require('socket.io-client');
+const faker = require('faker');
+let host = 'http://localhost:3030';
+
+// connect to the namespace:
+const capsConnection = io.connect(`${host}/caps`);
+
+// join a room with the store name, and emit a pickup event:
+
+
+
+
+// Emitting the pickup event:
+
+setInterval(() => {
+    let pickupLoad = {
+        store: faker.company.companyName(),
+        orderID: faker.datatype.uuid(),
+        customer: faker.name.findName(),
+        address: faker.address.streetAddress()
+    };
+
+    capsConnection.emit('joinRoom', pickupLoad.store);
+    // Listen for joining room:
+    capsConnection.on('joinedRoom', (payload) => {
+        console.log(payload);
+        capsConnection.emit('pickup', pickupLoad);
+    });
+}, 5000)
+
+
+// <-- Listening -->
+capsConnection.on('delivered', (payload) => {
+    setTimeout(() => {
+        console.log(`VENDOR: Thank you for delivering ${payload.orderID}`);
+    }, 1000);
+});
